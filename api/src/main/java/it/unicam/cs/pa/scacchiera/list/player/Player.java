67bbc0361f7e8d496d778afcb20d08bf7adcdff6
loris minetti <lorisminetti1997@gmail.com>
2023-03-01 00:10:51 +0100

    ComputerPlayer implemented

--- api/src/main/java/it/unicam/cs/pa/scacchiera/list/player/Player.java
+++ api/src/main/java/it/unicam/cs/pa/scacchiera/list/player/Player.java
@@ -1,5 +1,9 @@
 package it.unicam.cs.pa.scacchiera.list.player;
 
+import it.unicam.cs.pa.scacchiera.list.GameFrame;
+import it.unicam.cs.pa.scacchiera.list.Location;
+import it.unicam.cs.pa.scacchiera.list.Move;
+import it.unicam.cs.pa.scacchiera.list.pieces.Piece;
 import it.unicam.cs.pa.scacchiera.list.util.Colour;
 
 /**
@@ -17,6 +21,10 @@ public class Player {
     public Player(String name, Colour colour) {
         this.name = name;
         this.colour = colour;
+    }
+
+    public Move play(GameFrame<Piece, Location> frame) {
+        return null;
     }
 
     public String getName() {
