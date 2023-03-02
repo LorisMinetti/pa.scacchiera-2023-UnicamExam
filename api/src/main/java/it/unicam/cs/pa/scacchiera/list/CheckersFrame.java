6ffb6e89d661df9e5223e99d4a98c241ee5a8043
LorisMinetti <110096152+lorisminettiICCS@users.noreply.github.com>
2023-03-02 17:41:33 +0100

    capture boolean in Move added -- is Necessary?

--- api/src/main/java/it/unicam/cs/pa/scacchiera/list/CheckersFrame.java
+++ api/src/main/java/it/unicam/cs/pa/scacchiera/list/CheckersFrame.java
@@ -45,8 +45,9 @@ public class CheckersFrame implements GameFrame<Piece, Location> {
         return future;
     }
     /**
-     * Lista di tutte le posibili mosse che un pezzo, appartenente ad un giocatore specifico, può effettuare.
+     * Lista di tutte le posibili mosse che un pezzo, appartenente ad un giocatore specifico, può effettuare
      *
+     * ATTENZIONE: se questa lista torna vuota la partita deve terminare !!
      * @param state
      * @param col
      * @param piece
@@ -55,67 +56,100 @@ public class CheckersFrame implements GameFrame<Piece, Location> {
     @Override
     public List<Move> allPieceMoves(GameFrame<Piece, Location> state, Colour col, Piece piece) {
         List<Move> moveList = new ArrayList<>();
+        /*questa è una lista "speciale" ovvero la lista delle sole mosse di cattura, poichè se un pezzo sia re o pedina che sia,
+        * si trova davanti a una cattura è obbligato a eseguirla. Non verranno dunque più prese in considerazione le altre.  */
+        List<Move> captureList = new ArrayList<>();
         List<Location> diagonalSpots = getTheBoard().getDiagonalAdjacentLocationsOfPiece(piece);
         Pawn pawn = new Pawn(piece.getLocation(), piece.getColour());
-        pawnOrKing(col, moveList, diagonalSpots, pawn);
-        return moveList;
-    }
-
-    private void pawnOrKing(Colour col, List<Move> moveList, List<Location> diagonalSpots, Pawn pawn) {
-        for (Location loc : diagonalSpots) {
-            /* Check nella cell destinataria c'è un pezzo avversario */
-            if (pawn.getColour() != col) {
-                if(pawn.isKing()) {     /* Caso in cui la pedina è un re. Può muovere ovunque senza restrizioni*/
-                    Location nextDiagonalSpot = getTheBoard().getNextDiagonalSpot(pawn.getLocation(), loc);
-                    moveList.add(new Move(pawn.getLocation(), nextDiagonalSpot));
+        for(Location loc : diagonalSpots){
+            if(loc.getPiece().isPresent()){   //controllo se c'è un pezzo in una delle caselle diagonali
+                if(loc.getPiece().get().getColour() != pawn.getColour()     //controllo se il pezzo è dell'altro giocatore
+                        && getTheBoard().getNextDiagonalSpot(pawn.getLocation(), loc).isFree()){    //e parallelamente se la cella diagonalmente successiva è libera
+                        captureList.add(new Move(pawn.getLocation(), getTheBoard().getNextDiagonalSpot(pawn.getLocation(), loc), true));
                 }
-                else {   /* Caso in cui la pedina non è re*/
-                    unidriectionalPawnMove(moveList, loc, pawn);
-                }
-            }
-            /* Check se la casella destinataria è vuota ed è nella scacchiera */
+            } else { //non c'è nessun altro pezzo presente
                 moveList.add(new Move(pawn.getLocation(), loc));
+            }
         }
+        return switchList(moveList, captureList);
     }
 
     /**
-     * Metodo che gestisce i movimenti delle pedine semplici di ogni giocatore nell'unica direzione possibile in base alla loro posizione di partenza.
-     * @param moveList
-     * @param loc
-     * @param pawn
+     * Se la prima lista è vuota, restituisce la seconda
+     * @param moveList lista
+     * @param captureList lista alternativa
+     * @return prima lista non vuota.
      */
-    private void unidriectionalPawnMove(List<Move> moveList, Location loc, Pawn pawn) {
-        int increment = (pawn.getColour() == Colour.BLACK) ? 1 : -1;         /* costante che incrementa o decrementa la riga in base al colore */
-        int eatIncrement = (pawn.getColour() == Colour.BLACK) ? 2 : -2;      /* costante di controllo per una cattura */
-        if(pawn.getColour() == Colour.BLACK){                                       /* Se è nera può solo andare verso il basso*/
-            boolean canMove = ((loc.getRow() - pawn.getLocation().getRow()) == increment )    /* Check se l'incremento verticale */
-                    || ((loc.getRow() - pawn.getLocation().getRow()) == eatIncrement );
-            if(canMove && getTheBoard().isInsideBoard(loc)){                    /* Se la distanza tra le due coordinate  */
-                moveList.add(new Move(pawn.getLocation(), loc));
-            }
-        } else if( pawn.getColour() == Colour.WHITE){   /* Se è bianca può solo andare verso l'alto */
-            boolean canMove = (loc.getRow() - pawn.getLocation().getRow()) == increment
-                    || ((loc.getRow() - pawn.getLocation().getRow()) == eatIncrement );
-            if(canMove && getTheBoard().isInsideBoard(loc)){
-                moveList.add(new Move(pawn.getLocation(), loc));
-            }
-        }
+    private static List<Move> switchList(List<Move> moveList, List<Move> captureList) {
+        if(!captureList.isEmpty()){
+            return captureList;
+        } else
+            return moveList;
     }
+
+//    /**
+//     * metodo che controlla se si tratta di una pedina o di un re.
+//     * @param col
+//     * @param moveList
+//     * @param diagonalSpots
+//     * @param pawn
+//     */
+//    private void pawnOrKing(Colour col, List<Move> moveList, List<Location> diagonalSpots, Pawn pawn) {
+//        for (Location loc : diagonalSpots) {
+//            /* Check nella cell destinataria c'è un pezzo avversario */
+//            if (pawn.getColour() != col) {
+//                if(pawn.isKing()) {     /* Caso in cui la pedina è un re. Può muovere ovunque senza restrizioni*/
+//                    Location nextDiagonalSpot = getTheBoard().getNextDiagonalSpot(pawn.getLocation(), loc);
+//                    moveList.add(new Move(pawn.getLocation(), nextDiagonalSpot));
+//                }
+//                else {   /* Caso in cui la pedina non è re*/
+//                    unidriectionalPawnMove(moveList, loc, pawn);
+//                }
+//            }
+//            /* Check se la casella destinataria è vuota ed è nella scacchiera */
+//                moveList.add(new Move(pawn.getLocation(), loc));
+//        }
+//    }
+//
+//    /**
+//     * Metodo che gestisce i movimenti delle pedine semplici di ogni giocatore nell'unica direzione possibile in base alla loro posizione di partenza.
+//     * @param moveList
+//     * @param loc
+//     * @param pawn
+//     */
+//    private void unidriectionalPawnMove(List<Move> moveList, Location loc, Pawn pawn) {
+//        int increment = (pawn.getColour() == Colour.BLACK) ? 1 : -1;         /* costante che incrementa o decrementa la riga in base al colore */
+//        int eatIncrement = (pawn.getColour() == Colour.BLACK) ? 2 : -2;      /* costante di controllo per una cattura */
+//        if(pawn.getColour() == Colour.BLACK){                                       /* Se è nera può solo andare verso il basso*/
+//            boolean canMove = ((loc.getRow() - pawn.getLocation().getRow()) == increment )    /* Check se l'incremento verticale */
+//                    || ((loc.getRow() - pawn.getLocation().getRow()) == eatIncrement );
+//            if(canMove && getTheBoard().isInsideBoard(loc)){                    /* Se la distanza tra le due coordinate  */
+//                moveList.add(new Move(pawn.getLocation(), loc));
+//            }
+//        } else if( pawn.getColour() == Colour.WHITE){   /* Se è bianca può solo andare verso l'alto */
+//            boolean canMove = (loc.getRow() - pawn.getLocation().getRow()) == increment
+//                    || ((loc.getRow() - pawn.getLocation().getRow()) == eatIncrement );
+//            if(canMove && getTheBoard().isInsideBoard(loc)){
+//                moveList.add(new Move(pawn.getLocation(), loc));
+//            }
+//        }
+//    }
 
     /**
      * Tutte le possibili mosse per un giocatore durante un determinato momento della partita.
-     *
-     * @param state
      * @param colour
      * @return lista delle mosse possibili.
      */
     @Override
-    public List<Move> allPossibleMoves(GameFrame<Piece, Location> state, Colour colour) {
+    public List<Move> allPossibleMoves(Colour colour) {
         List<Move> possibleMoves = new ArrayList<>();
         for (Location loc : getTheBoard().getAllLocationsOfPlayer(colour)) {
-            Piece pz = loc.getPiece().get();
+            if(loc.getPiece().isPresent()){
+                Piece pz = loc.getPiece().get();
+                List<Move> movesOfPiece = allPieceMoves(this, colour, pz);
+                possibleMoves.addAll(movesOfPiece);
+            }
             /* Concateno tutte le mosse per ogni pezzo per ottenere tutte le mosse di uno specifico giocatore */
-            possibleMoves.addAll(allPieceMoves(this, colour, pz));
         }
         return possibleMoves;
     }
@@ -148,7 +182,7 @@ public class CheckersFrame implements GameFrame<Piece, Location> {
         StringBuilder sb = new StringBuilder();
         sb.append("Pezzi disponibili da muovere: ");
         for(Piece piece : unblockedPieces()){
-            sb.append(piece + " - ");
+            sb.append(piece).append(" - ");
         }
         return sb.toString();
     }
