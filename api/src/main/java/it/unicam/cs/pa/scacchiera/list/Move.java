3f581e0fa1ba41bc27663201210c34b47052f0b4
LorisMinetti <110096152+lorisminettiICCS@users.noreply.github.com>
2023-03-02 17:41:40 +0100

    capture boolean in Move added -- is Necessary?

--- api/src/main/java/it/unicam/cs/pa/scacchiera/list/Move.java
+++ api/src/main/java/it/unicam/cs/pa/scacchiera/list/Move.java
@@ -3,10 +3,19 @@ package it.unicam.cs.pa.scacchiera.list;
 public class Move {
 
     private Location start, destination;
+    private boolean capture;
+    //TODO: per implementare il comportamento di dover mangiare obbligatoriamente quando c'è n'è la possibilità,
+    //implmentare un booleano implicito all'oogetto che tiene conto se la mossa è di cattura.
 
     public Move(Location start, Location dest) {
         this.start = start;
         destination = dest;
+        capture = false;
+    }
+    public Move(Location start, Location dest, boolean capture) {
+        this.start = start;
+        destination = dest;
+        this.capture = capture;
     }
 
     Location getStart() {
