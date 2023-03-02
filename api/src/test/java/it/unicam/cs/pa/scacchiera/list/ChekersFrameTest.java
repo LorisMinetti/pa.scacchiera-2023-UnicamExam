5bdcada45654866690c74c2d5b029f5412e7ff6a
LorisMinetti <110096152+lorisminettiICCS@users.noreply.github.com>
2023-03-02 17:40:55 +0100

    checkersframe tests

--- api/src/test/java/it/unicam/cs/pa/scacchiera/list/ChekersFrameTest.java
+++ api/src/test/java/it/unicam/cs/pa/scacchiera/list/ChekersFrameTest.java
@@ -31,35 +31,46 @@ public class ChekersFrameTest {
     public void allPieceMoves() throws Exception {
         // Setup
         Board<Piece, Location> board = new CheckersBoard(8, 8);
-        Piece whitePiece = new Pawn( board.getSchema()[1][1], Colour.WHITE);
-        board.setPiece(whitePiece.getLocation(), whitePiece);
-
         CheckersFrame checkersFrame = new CheckersFrame(null, Colour.WHITE, board);
 
+        Piece pezzo1 = checkersFrame.getTheBoard().getPiece(5, 1);
+        Piece pezzo2 = checkersFrame.getTheBoard().getPiece(5, 7);
+        Piece pezzo3 = checkersFrame.getTheBoard().getPiece(2, 0);
+        Piece pezzo4 = checkersFrame.getTheBoard().getPiece(2, 4);
+
         // Test
-        List<Move> moves = checkersFrame.allPieceMoves(checkersFrame, Colour.WHITE, whitePiece);
+        List<Move> moves1 = checkersFrame.allPieceMoves(checkersFrame, Colour.WHITE, pezzo1);
+        List<Move> moves2 = checkersFrame.allPieceMoves(checkersFrame, Colour.WHITE, pezzo2);
+        List<Move> moves3 = checkersFrame.allPieceMoves(checkersFrame, Colour.WHITE, pezzo3);
+        List<Move> moves4 = checkersFrame.allPieceMoves(checkersFrame, Colour.WHITE, pezzo4);
 
         // Verify
-        assertEquals(2, moves.size());
-        assertTrue(moves.contains(new Move(whitePiece.getLocation(), board.getSchema()[0][0])));
-        assertTrue(moves.contains(new Move(whitePiece.getLocation(),  board.getSchema()[2][0])));
+        assertEquals(2, moves1.size());
+        assertEquals(2, moves4.size());
+        assertEquals(1, moves2.size());
+        assertEquals(1, moves3.size());
+
+        Location l1 = new LocationImpl(5, 1);
+        Location l2 = new LocationImpl(5, 7);
+        Location l3 = new LocationImpl(2, 0);
+        Location l4 = new LocationImpl(2, 4);
+        assertTrue(moves1.contains( new Move(l1, board.getSchema()[4][0] )));
+        assertTrue(moves1.contains( new Move(l1, board.getSchema()[4][2] )));
+
     }
 
     @Test
     public void allPossibleMoves() throws Exception {
         // Setup
         Board<Piece, Location> board = new CheckersBoard(8, 8);
-        Piece whitePiece = new Pawn( board.getSchema()[1][1], Colour.WHITE);
-        board.setPiece(whitePiece.getLocation(), whitePiece);
-
         CheckersFrame checkersFrame = new CheckersFrame(null, Colour.WHITE, board);
 
         // Test
-        List<Move> moves = checkersFrame.allPossibleMoves(checkersFrame, Colour.WHITE);
+        List<Move> moves = checkersFrame.allPossibleMoves(Colour.WHITE);
+        List<Move>  blackMoves = checkersFrame.allPossibleMoves(Colour.BLACK);
 
         // Verify
-        assertEquals(2, moves.size());
-        assertTrue(moves.contains(new Move(whitePiece.getLocation(), board.getSchema()[0][0])));
-        assertTrue(moves.contains(new Move(whitePiece.getLocation(),  board.getSchema()[2][0])));
+        assertEquals(7, moves.size());
+        assertEquals(7, blackMoves.size());
     }
 }
